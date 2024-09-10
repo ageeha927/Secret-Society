c25ef50dff693ca8ee3fbc22261f5306eb1d022c
ageeha927 <ageeha927@west-mec.org>
2024-09-09 22:39:36 -0700

    .

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
-const http = require('http');
-const fs = require('fs');
+const http = require('http')
+const fs = require('fs')
 
 http
     .createServer((req, res) => {
         // Conditional Rendering
         switch (req.url) {
             case '/': 
-                // Home
-                fs.readFile("./index.html", (err, data) => {
-                    res.end(data);
+                fs.readFile("index.html", (err, data) => {
+                    res.end(data)
                 });
-                break;
+                break
+            case '/6': 
+                fs.readFile("puzz1.html", (err, data) => {
+                    res.end(data)
+                });
+                break
+            case '/yes': 
+                fs.readFile("puzz2.html", (err, data) => {
+                    res.end(data)
+                });
+                break
+            case '/yellow': 
+                fs.readFile("puzz3.html", (err, data) => {
+                    res.end(data)
+                });
+            break
+            case '/thebeemovie': 
+                fs.readFile("puzz4.html", (err, data) => {
+                    res.end(data)
+                });
+            break
             }
-    })
+    }).listen(3000)
